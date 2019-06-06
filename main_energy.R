e8277a169131025071a13f3df74562981ff69424
Paul Bui Quang <paul@tenpoint7.com>
2019-06-06 23:35:55 +0700

    Update

--- main_energy.R
+++ main_energy.R
@@ -18,6 +18,10 @@
 # Value-at-Risk and Expected Shortfall are needed for 1%, 2.5%, 5%, 95%, 97.5% and 99%,
 # where Value-at-Risk is the quantile and Expected Shortfall is the mean of the returns exceeding the quantile.
 
+# Asset names: 
+# "WTI", "Brent", "Gasoline_NYH", "Gasoline_US_Gulf", "NYHNo2HeatingOil", "LA_Diesel",
+# "USGulf_Kerosine", "Propane", "HenryHubNG"
+
 rm(list=ls())
 set.seed(99)
 library(zoo)
@@ -25,8 +29,9 @@ library(zoo)
 ### settings ###
 
 path = '/home/paul/code/SV_SMC/'
-model = 'SV'
-freq = 'week'
+model = 'ASV'
+freq = 'month'
+asset = 'Brent'
 
 # out-of-sample starting time
 start.out = as.POSIXct('2000-01-01', origin='1970-01-01', tz='GMT')
@@ -64,7 +69,7 @@ if (freq=='day') {
   maxiter = 20
   assign('data', SPOT.dRet)
 } else if (freq=='week') {
-  fit.period = 30
+  fit.period = 12
   maxiter.init = 100
   maxiter = 10
   assign('data', SPOT.wRet)
@@ -86,35 +91,33 @@ data.0 = data[index(data) >= start.in]
 forecast.index = index(data.0)[index(data.0) >= start.out]
 T.train = length(index(data.0)[index(data.0) < start.out])
 
-for (j in 1:ncol(data)) {
+# asset index
+j = which(attr(data, 'dimnames')[[2]] == asset)
 
-  # model fitting, particle forecasting
-  series = coredata(data.0[,j])
-  param.init = initialize.parameters(series)
-  series.forecast = run.experiment.SV(N, Nth, 1, param.init, param.inf, param.sup,
-                                     T.train, series, maxiter.init, maxiter, tol, fit.period)
+# model fitting, particle forecasting
+series = coredata(data.0[,j])
+param.init = initialize.parameters(series)
+series.forecast = run.experiment.SV(N, Nth, 1, param.init, param.inf, param.sup,
+                                    T.train, series, maxiter.init, maxiter, tol, fit.period)
   
-  # PIT, VaR, ES
-  stats = forecast.statistics(series[index(data.0) >= min(forecast.index)], series.forecast)
-  PIT = stats$PIT
-  VaR.01 = stats$VaR.01; VaR.025 = stats$VaR.025; VaR.05 = stats$VaR.05
-  VaR.95 = stats$VaR.95; VaR.975 = stats$VaR.975; VaR.99 = stats$VaR.99
-  ES.01 = stats$ES.01; ES.025 = stats$ES.025; ES.05 = stats$ES.05
-  ES.95 = stats$ES.95; ES.975 = stats$ES.975; ES.99 = stats$ES.99
+# PIT, VaR, ES
+stats = forecast.statistics(series[index(data.0) >= min(forecast.index)], series.forecast)
+PIT = stats$PIT
+VaR.01 = stats$VaR.01; VaR.025 = stats$VaR.025; VaR.05 = stats$VaR.05
+VaR.95 = stats$VaR.95; VaR.975 = stats$VaR.975; VaR.99 = stats$VaR.99
+ES.01 = stats$ES.01; ES.025 = stats$ES.025; ES.05 = stats$ES.05
+ES.95 = stats$ES.95; ES.975 = stats$ES.975; ES.99 = stats$ES.99
   
-  # result dataframe (model x asset x frequency)
-  df = data.frame(matrix(ncol=14, nrow=length(forecast.index)))
-  asset.name = attr(data, 'dimnames')[[2]][j]
-  colnames(df) = c(asset.name, 'PIT',
-                   'VaR.01', 'VaR.025', 'VaR.05', 'VaR.95', 'VaR.975', 'VaR.99',
-                   'ES.01', 'ES.025', 'ES.05', 'ES.95', 'ES.975', 'ES.99')
-  row.names(df) = forecast.index
-  df[asset.name] = series[(T.train+1):length(series)]; df['PIT'] = PIT
-  df['VaR.01'] = VaR.01; df['VaR.025'] = VaR.025; df['VaR.05'] = VaR.05
-  df['VaR.95'] = VaR.95; df['VaR.975'] = VaR.975; df['VaR.99'] = VaR.99
-  df['ES.01'] = ES.01; df['ES.025'] = ES.025; df['ES.05'] = ES.05
-  df['ES.95'] = ES.95; df['ES.975'] = ES.975; df['ES.99'] = ES.99
-  name.df = paste(model, asset.name, freq, sep='__')
-  save(df, file=paste(path.data, name.df, '.RData', sep=''))
-  
-}
+# result dataframe (model x asset x frequency)
+df = data.frame(matrix(ncol=14, nrow=length(forecast.index)))
+colnames(df) = c(asset, 'PIT',
+                 'VaR.01', 'VaR.025', 'VaR.05', 'VaR.95', 'VaR.975', 'VaR.99',
+                 'ES.01', 'ES.025', 'ES.05', 'ES.95', 'ES.975', 'ES.99')
+row.names(df) = forecast.index
+df[asset] = series[(T.train+1):length(series)]; df['PIT'] = PIT
+df['VaR.01'] = VaR.01; df['VaR.025'] = VaR.025; df['VaR.05'] = VaR.05
+df['VaR.95'] = VaR.95; df['VaR.975'] = VaR.975; df['VaR.99'] = VaR.99
+df['ES.01'] = ES.01; df['ES.025'] = ES.025; df['ES.05'] = ES.05
+df['ES.95'] = ES.95; df['ES.975'] = ES.975; df['ES.99'] = ES.99
+name.df = paste(model, asset, freq, sep='__')
+save(df, file=paste(path.data, name.df, '.RData', sep=''))
