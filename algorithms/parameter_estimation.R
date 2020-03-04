a636c34ff2db1aaabef5b03d453c77da3dc3160f
Paul Bui Quang <paul@tenpoint7.com>
2020-03-04 20:50:34 +0700

    Add code file for gradient-based parameter estimation function

---
+++ algorithms/parameter_estimation.R
@@ -0,0 +1,27 @@
+batch_parameter_estimation = function(N, Nth, y, thres, maxiter=100) {
+  # Batch likelihood maximization.
+  last.param = initialize.parameters(y)
+  params = param
+  iter = 0
+  while (rate > thres & iter < maxiter) {
+    filter = info.particle.filter(N, Nth, y, last.param)
+    scorinfo = compute.score.info(filter$weights, filter$alphas, filter$betas)
+    score = scorinfo$score
+    info = scorinfo$info
+    new.param = last.param - 0.9 * inv(info) %*% score
+    params = rbind(new.param)
+    rate = sqrt(sum((new.param - last.param)**2) / sum(last.param**2))
+    iter = iter + 1
+    last.param = new.param
+  }
+  return(params)
+}
+
+recursive_parameter_estimation = function(N, Nth, y, last.param, last.score, thres, maxiter=100) {
+  # Recursive likelihood maximization.
+  
+  filter = info.particle.filter(N, Nth, y, last.param)
+  scorinfo = compute.score.info(filter$weights, filter$alphas, filter$betas)
+  score = scorinfo$score
+  param = last.param + 0.9 * score - 0.9 * last.score
+}
